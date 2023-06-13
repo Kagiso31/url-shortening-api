65b1a6bb71aaadeb8366a9103b914d889be63f97
Kagiso31 <kagisomokou@gmail.com>
2023-06-13 13:19:20 +0200

    Add delete button

--- src/components/ShortenLinkItem.jsx
+++ src/components/ShortenLinkItem.jsx
@@ -15,6 +15,15 @@ const ShortenLinkItem = ({ id, originalLink, shortLink, deleteListItem }) => {
 
   return (
     <li className="shorten-link-item">
+      <button className="delete-btn" onClick={() => deleteListItem(id)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+          <path
+            fill="#494C6B"
+            fillRule="evenodd"
+            d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+          />
+        </svg>
+      </button>
       <a className="original-link">{originalLink}</a>
       <hr />
       <a className="shortened-link" href={shortLink} target="_blank">
